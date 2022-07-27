b8de803b9c5ac399bbb2bbffca46096392151a9a
Lexus Banton <lexcancode@gmail.com>
2022-07-26 21:33:16 -0700

    website

--- app.js
+++ app.js
@@ -1,8 +1,76 @@
 // import functions and grab DOM elements
+const cafeButton = document.getElementById('cafe-button');
+const arcadeButton = document.getElementById('arcade-button');
+const bookstoreButton = document.getElementById('bookstore-button');
+
+const cafeContainer = document.getElementById('cafe-container');
+const arcadeContainer = document.getElementById('arcade-container');
+const bookstoreContainer = document.getElementById('bookstore-container');
+
+const winsEl = document.getElementById('wins');
+const totalEl = document.getElementById('total');
+const lossesEl =document.getElementById('losses');
 
 // let state
+const hidingPlaces = ['cafe' , 'arcade' , 'bookstore'];
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
+let correctGuesses = 0;
+let totalGuesses =0 ;
+
+
+function getRandomItem(arr){
+  const index = Math.floor(Math.random()*arr.length);
+
+  return arr[index];
+}
+
+// set event listeners
+
+cafeButton.addEventListener('click', () => {
+  const hidingSpot = Math.floor(Math.random() * 3);
+  const answer = hidingPlaces[hidingSpot];
+  handleGuess(answer, 'cafe');
+});
+
+arcadeButton.addEventListener('click', () => {
+  const hidingSpot = Math.floor(Math.random() * 3);
+  const answer = hidingPlaces[hidingSpot];
+  handleGuess(answer, 'arcade');
+});
+
+bookstoreButton.addEventListener('click', () => {
+  const hidingSpot = Math.floor(Math.random() * 3);
+  const answer = hidingPlaces[hidingSpot];
+  handleGuess(answer, 'bookstore');
+});
+
+// set reset styles
+
+function resetStyles() {
+  cafeContainer.classList.remove('taliah');
+  arcadeContainer.classList.remove('taliah');
+  bookstoreContainer.classList.remove('taliah');
+}
+
+function handleGuess(correctSpot, userGuess) {
+  resetStyles();
+
+
+  const correctHidingPlaceEl = document.getElementById(`${correctSpot}-container`);
+// increment guesses
+  totalGuesses++;
+
+const taliah = document.getElementById(`${correctSpot}-container`);
+    taliah.classList.add('taliah');
+
+   //if correct
+   
+   if(userGuess === correctSpot){
+    correctGuesses++;
+   }
+
   // update DOM to reflect the new state
+  totalEl.textContent = totalGuesses;
+  lossesEl.textContent = totalGuesses- correctGuesses;
+  winsEl.textContent = correctGuesses;
+}
